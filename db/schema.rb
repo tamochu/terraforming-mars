0c8b759f1f60d52415086189510731a3728a88ca
tamochu <tamochu.mobile@gmail.com>
2020-08-09 09:35:41 +0000

    test pass

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,19 @@ ActiveRecord::Schema.define(version: 20200809090010) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name"
+    t.string "password"
+    t.string "provider"
+    t.string "uid"
+    t.string "meta"
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.string "reset_password_sent_at"
+    t.string "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,10 +8,5 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", signup_path
-    
-    get signup_path
-    assert_template 'users/new'
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", about_path
   end
 end

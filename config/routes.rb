739cff06fb00939bc8a3acd8b38015a2d76cd7cd
tamochu <tamochu.mobile@gmail.com>
2020-08-09 17:21:13 +0000

    add log_out

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
   }
+  devise_scope :user do
+    delete '/log_out', to: 'devise/sessions#destroy'
+  end
+  
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
 end

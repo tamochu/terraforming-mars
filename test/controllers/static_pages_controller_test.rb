f4e31bf2181bf57cfbdc1b52880b08104aa06c17
tamochu <tamochu.mobile@gmail.com>
2020-08-09 04:51:24 +0000

    static pages

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | テラフォーミングマーズ"
+  end
+
+end

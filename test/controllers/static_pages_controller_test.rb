e2ef1c82ff00cd1201ab2360320f4363408d8f42
tamochu <tamochu.mobile@gmail.com>
2020-08-09 08:26:53 +0000

    header layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,15 +2,15 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
-
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
     assert_select "title", "Home | テラフォーミングマーズ"
   end
-
+  
+  test "should get about" do
+    get about_path
+    assert_response :success
+    assert_select "title", "About | テラフォーミングマーズ"
+  end
 end
